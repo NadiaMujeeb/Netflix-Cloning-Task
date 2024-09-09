a8b7c0cce571ae8a866c9ce0849d26fcdea40212
Nadia Mujeeb <nadia.mujeeb@meridianit.com>
2024-09-09 23:45:34 +0530

    feature responsive

--- src/components/FeatureItem.tsx
+++ src/components/FeatureItem.tsx
@@ -1,3 +1,29 @@
+// import React from 'react';
+
+// interface FeatureItemProps {
+//   title: string;
+//   description: string;
+//   icon: string;
+// }
+
+// const FeatureItem: React.FC<FeatureItemProps> = ({ title, description, icon }) => {
+//   return (
+//     <div className=" relative h-full  flex-col  text-center bg-gradient-to-r from-purple-900 to-blue-950 p-8 pb-16 rounded-2xl flex justify-between items-center text-white mb-4 w-[280px]">
+//       <div>
+//         <h3 className="text-xl font-bold mb-2 ">{title}</h3>
+//         <p>{description}</p>
+//       </div>
+//       <div>
+//         <img src={icon} alt={title} className="w-15 h-11 absolute bottom-0 right-0 mr-6 mb-6" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FeatureItem;
+
+
+
 import React from 'react';
 
 interface FeatureItemProps {
@@ -8,7 +34,7 @@ interface FeatureItemProps {
 
 const FeatureItem: React.FC<FeatureItemProps> = ({ title, description, icon }) => {
   return (
-    <div className=" relative bg-gradient-to-r from-purple-900 to-blue-950 px-4 pt-3 pb-12 rounded-2xl flex justify-between items-center text-white mb-4 w-[400px] h-[280px] m-1">
+    <div className="relative bg-gradient-to-r from-purple-900 to-blue-950 px-4 pt-12 sm:pt-3 pb-28 sm:pb-12 rounded-2xl flex justify-between items-center text-white w-full h-[180px] sm:h-[270px] m-0 sm:m-1 ">
       <div>
         <h3 className="text-xl font-bold mb-2 ">{title}</h3>
         <p>{description}</p>
@@ -21,3 +47,4 @@ const FeatureItem: React.FC<FeatureItemProps> = ({ title, description, icon }) =
 };
 
 export default FeatureItem;
+
