57d83e522fe01bcef3d31930c48b85b2cb4055b5
Nadia Mujeeb <nadia.mujeeb@meridianit.com>
2024-09-10 15:10:43 +0530

    slider

--- src/components/SliderSection.tsx
+++ src/components/SliderSection.tsx
@@ -30,7 +30,14 @@ const sliderSettings = {
       },
     },
     {
-      breakpoint: 430, 
+      breakpoint: 1024, 
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      },
+    },
+    {
+      breakpoint: 768, 
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
