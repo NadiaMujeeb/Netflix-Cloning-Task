4988ec4ddf9e1a988bf903abb9d99a4842c60606
Nadia Mujeeb <nadia.mujeeb@meridianit.com>
2024-09-10 11:17:50 +0530

    features fro ipad air

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -1,3 +1,41 @@
+// import React from 'react';
+// import FeatureItem from './FeatureItem';
+// import profile from "../assets/profile__logo.png";
+
+// const FeaturesSection: React.FC = () => {
+//   return (
+//     <div className="bg-black p-4 sm:p-8 lg:mx-[120px] mb-8">
+//       <h2 className="text-white text-lg sm:text-2xl mb-4 sm:mb-8 font-bold">
+//         More reasons to join
+//       </h2>
+//       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 " >
+//         <FeatureItem
+//           title="Enjoy on your TV"
+//           description="Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+//           icon={profile} 
+//         />
+//         <FeatureItem
+//           title="Download your shows to watch offline"
+//           description="Save your favorites easily and always have something to watch."
+//           icon={profile} 
+//         />
+//         <FeatureItem
+//           title="Watch everywhere"
+//           description="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
+//           icon={profile}  
+//         />
+//         <FeatureItem
+//           title="Create profiles for kids"
+//           description="Send kids on adventures with their favourite characters in a space made just for them "
+//           icon={profile} 
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FeaturesSection;
+
 import React from 'react';
 import FeatureItem from './FeatureItem';
 import profile from "../assets/profile__logo.png";
@@ -8,7 +46,7 @@ const FeaturesSection: React.FC = () => {
       <h2 className="text-white text-lg sm:text-2xl mb-4 sm:mb-8 font-bold">
         More reasons to join
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 " >
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-2 xl:grid-cols-4">
         <FeatureItem
           title="Enjoy on your TV"
           description="Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
@@ -26,7 +64,7 @@ const FeaturesSection: React.FC = () => {
         />
         <FeatureItem
           title="Create profiles for kids"
-          description="Send kids on adventures with their favourite characters in a space made just for them "
+          description="Send kids on adventures with their favourite characters in a space made just for them."
           icon={profile} 
         />
       </div>
@@ -35,4 +73,3 @@ const FeaturesSection: React.FC = () => {
 };
 
 export default FeaturesSection;
-
