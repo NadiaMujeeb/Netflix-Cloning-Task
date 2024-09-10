e9a9a4af5e398af70673eac29694bf55289fb8b3
Nadia Mujeeb <nadia.mujeeb@meridianit.com>
2024-09-10 13:48:06 +0530

    slider

--- src/components/SliderItem.tsx
+++ src/components/SliderItem.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 interface SliderItemProps {
-    title: string;
-    num:number;
-    icon: string;
-  }
-  
-  const SliderItem: React.FC<SliderItemProps> = ({ title, icon ,num}) => {
-    return (
-      <div className="relative ">
+  title: string;
+  num: number;
+  icon: string;
+}
 
-       <div className='flex '>
-          <img src={icon} alt={title} className="w-44 h-64 mb-6 rounded-lg" />
-          <span className="absolute left-[-10%] bottom-9  text-black px-2 py-1 rounded-full text-8xl font-bold" style={{
-    textShadow: '0 0 2px white, 0 0 2px white, 0 0 2px white, 0 0 2px white'
-  }}>{num}</span>
-        </div>
-      </div>
-    );
-  };
-  
-  export default SliderItem;
+const SliderItem: React.FC<SliderItemProps> = ({ title, icon, num }) => {
+  return (
+    <div className="relative flex items-center justify-center p-2">
+      <img 
+        src={icon} 
+        alt={title} 
+        className="w-36 h-44 mb-6 rounded-lg 
+          sm:w-32 sm:h-48 md:w-36 md:h lg:w-40 lg:h-56 
+          xl:w-48 xl:h-72"
+      />
+      <span 
+        className="absolute flex items-center justify-center text-black px-2 py-1 rounded-full font-bold 
+          text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl"
+        style={{ 
+          top: '55%', 
+          left: '-10%', 
+          textShadow: '0 0 2px white, 0 0 2px white, 0 0 2px white, 0 0 2px white' 
+        }}
+      >
+        {num}
+      </span>
+    </div>
+  );
+};
+
+export default SliderItem;
